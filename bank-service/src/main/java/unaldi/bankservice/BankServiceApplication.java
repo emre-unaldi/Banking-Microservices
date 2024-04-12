2ab1bb8b80c4b4c5a734b538f786f7e7171b206f
emre-unaldi <emree.unaldi@gmail.com>
2024-04-12 04:09:08 +0300

    Bank service, api gateway configured

--- bank-service/src/main/java/unaldi/bankservice/BankServiceApplication.java
+++ bank-service/src/main/java/unaldi/bankservice/BankServiceApplication.java
@@ -2,8 +2,10 @@ package unaldi.bankservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BankServiceApplication {
 
     public static void main(String[] args) {
