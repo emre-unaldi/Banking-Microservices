efe59050e23502b5e2c0570f7b04728c26ae3027
emre-unaldi <emree.unaldi@gmail.com>
2024-04-12 04:01:34 +0300

    User service, api gateway configured

--- user-service/src/main/java/unaldi/userservice/UserServiceApplication.java
+++ user-service/src/main/java/unaldi/userservice/UserServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
